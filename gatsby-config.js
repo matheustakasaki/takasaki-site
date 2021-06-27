ef1a205eea512c35d8ed95884cbacd3c947af518
matheustakasaki <matheus8antunes@gmail.com>
2021-06-27 13:54:43 -0300

    Exluding Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
